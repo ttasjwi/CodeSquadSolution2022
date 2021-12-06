feef000862b2f2ff60250cd9524436ceb5308b0f
ttasjwi <ttasjwi920@gmail.com>
2021-12-06 23:49:54 +0900

    [1.19 revision] Main 클래스 - Prompt 클래스 분리 - Main - `main` 메서드 : Prompt 객체 생성, run() 메서드 호출 - Prompt : 실행 내용, 현재 구체적으로 실행 내용구현은 안 됨

---
+++ Prompt.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Prompt {
+
+
+    private static final String MAP_FILE_DIR = "Map.txt";
+    private static final File MAP_FILE = new File(MAP_FILE_DIR);
+
+    //소스파일에서 스테이지단위로 텍스트를 분리한다.
+    private static List<String> splitStageStr(File mapFile) {
+        List<String> stageStrSplits = new ArrayList<>();
+        try {
+            Scanner fileScanner = new Scanner(mapFile).useDelimiter("=+"); // 파일을 읽어와서, 문자열 1개 이상의 "="을 구분자로 하여 쪼개는 Scanner 생성
+            while(fileScanner.hasNext()) {
+                String stageStr = fileScanner.next().trim(); // "==="앞을 읽어오고 양끝의 공백(개행문자, 공백)을 제거한다.
+                stageStrSplits.add(stageStr); // List에 추가한다.
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("파일 읽기 실패");
+            System.exit(0);
+        }
+        return stageStrSplits; // List를 반환한다.
+    }
+
+    // 실행
+    public void run() {
+        printStages();
+    }
+
+    //TODO : 모든 스테이지들을 출력한다.
+    private void printStages() {
+        System.out.println("프로그램 실행 확인");
+        System.out.println("할 것 : 모든 스테이지를 출력할 것");
+    }
+
+}
