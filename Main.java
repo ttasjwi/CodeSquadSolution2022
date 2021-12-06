49db038b5694add7bf06ed330657babb7256be8b
ttasjwi <ttasjwi920@gmail.com>
2021-12-06 17:14:13 +0900

    텍스트파일의 내용을 스테이지단위로 쪼개는 메서드 생성 - 소스파일의 텍스트를 스테이지단위로 분리하여 List에 나눠 저장하도록 하는 메서드 `splitStageStr`생성   - 스캐너의 `.useDelimiter("=+")` 을 활용함   - 나눠진 문자열들의 양 끝의 공백을 `.trim()` 메서드를 이용하여 제거 -> 마지막의 구분자가 제거되고 남은 개행문자도 제거됨.

--- Main.java
+++ Main.java
@@ -1,10 +1,31 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     private static final String MAP_FILE_DIR = "Map.txt";
     private static final File MAP_FILE = new File(MAP_FILE_DIR);
 
-    public static void main(String[] args) {
-        System.out.println("아무 것도 안 함.");
+    //소스파일에서 스테이지단위로 텍스트를 분리한다.
+    private static List<String> splitStageStr(File mapFile) {
+        List<String> stageStrSplits = new ArrayList<>();
+        try {
+            Scanner fileScanner = new Scanner(mapFile).useDelimiter("=+"); // 파일을 읽어와서, 문자열 1개 이상의 "="을 구분자로 하여 쪼개는 Scanner 생성
+            while(fileScanner.hasNext()) {
+                String stageStr = fileScanner.next().trim(); // "==="앞을 읽어오고 양끝의 공백(개행문자, 공백)을 제거한다.
+                stageStrSplits.add(stageStr); // List에 추가한다.
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("파일 읽기 실패");
+            System.exit(0);
+        }
+        return stageStrSplits; // List를 반환한다.
     }
+
+    public static void main(String[] args) {
+        System.out.println("프로그램이 정상적으로 실행됨");
+    }
+
 }
