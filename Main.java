8187b78c8d0ae727077541ac55bf4c54162fe713
ttasjwi <ttasjwi920@gmail.com>
2021-12-06 17:05:23 +0900

    맵 가져오기 - 최상위 디렉토리의 `Map.txt`파일을 읽어와 File객체 `MAP_FILE` 생성 - 이를 static 상수로 한다.

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Main {
+    private static final String MAP_FILE_DIR = "Map.txt";
+    private static final File MAP_FILE = new File(MAP_FILE_DIR);
+
+    public static void main(String[] args) {
+        System.out.println("아무 것도 안 함.");
+    }
+}
