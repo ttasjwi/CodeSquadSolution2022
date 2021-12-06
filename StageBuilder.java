606104c419071bff4d92533fa22d3dbb9080c82e
ttasjwi <ttasjwi920@gmail.com>
2021-12-06 21:10:51 +0900

    [1.07 revision] 만들 StageMap의 가로 길이를 계산하는 메서드 calWidth 정의
    - 멤버 `stageMapSrc`를 읽어, 각 라인 문자열의 길이들의 최댓값을 반환

--- StageBuilder.java
+++ StageBuilder.java
@@ -20,4 +20,15 @@ public class StageBuilder {
         this.stageMapSrc = stageMapSrc;
     }
 
+    // 만들 스테이지 맵의 가로크기를 구하여 반환한다.
+    private int calWidth() {
+        int maxLength = this.stageMapSrc.get(0).length(); // 라인별 문자열의 길이의 최댓값
+        for (String stageMapLine : this.stageMapSrc) {
+            int currentLineLength = stageMapLine.length();
+            if (maxLength < currentLineLength) {
+                maxLength = currentLineLength;
+            }
+        }
+        return maxLength;
+    }
 }
