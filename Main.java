8e151ad8e5f9f7110dd456ba45a4bb78e5b5d54d
ttasjwi <ttasjwi920@gmail.com>
2021-12-07 19:05:02 +0900

    [2.18 revision] 사용자 입력기능 추가
    1. 작업상황
        - static 상수로 `PROMPT`를 생성함. 이 문자는 `"SOKOBAN > "`이 저장되어 있다.
        - `inputCommandQueue(Scanner sc)` 메서드 : 사용자로부터 라인단위로 명령을 입력받고 이를 Queue<String>으로 반환한다.
          - 사용자로부터 라인단위로 입력받은 문자열을 한자 한자 쪼개서, List화 한뒤 이를 기반으로 `Queue`를 생성함.
        - `execute` 메서드
          - 다른 과정은 동일하고, 사용자 입력을 받아 `Queue<String>`를 생성하도록 함.
          - 사용자 입력시 지정된 명령이 존재하지 않을 경우, 명령 수행을 시행하지 않고 다음 명령을 수행하도록 함.
        - Main 클래스
          - 프로그램이 종료되면 "bye~" 를 출력하도록 함.
    2. 동작
        - 스테이지 명, 초기 맵상태가 출력됨
        - 사용자 입력을 받음.
        - 사용자 입력을 순차적으로 수행하고 모든 명령이 수행되거나, Q명령이 들어오면 종료됨.

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Prompt p = new Prompt();
         p.execute();
+        System.out.println("bye~");
     }
 }
--- Prompt.java
+++ Prompt.java
@@ -7,6 +7,7 @@ public class Prompt {
     private static final String MAP_FILE_DIR = "Map.txt";
     private static final File MAP_FILE = new File(MAP_FILE_DIR);
     private static final List<Stage> stages = initStages();
+    private static final String PROMPT = "SOKOBAN > ";
     private boolean isPlaying = true;
 
     //소스파일에서 스테이지단위로 텍스트를 분리한다.
@@ -40,24 +41,27 @@ public class Prompt {
 
     // 실행
     public void execute() {
+        Scanner sc = new Scanner(System.in);
         Stage stage2 = stages.get(1);
         System.out.println(stage2.getStageName());
+        stage2.printStageMap();
 
+        System.out.print(PROMPT);
         Map<String, Runnable> commandMap = initCommandMap(stage2);
-        Queue<String> queue = new LinkedList<>();
-        queue.add("D");
-        queue.add("S");
-        queue.add("A");
-        queue.add("W");
-        queue.add("Q");
-        queue.add("S");
+        Queue<String> queue = inputCommandQueue(sc);
 
         while (isPlaying && queue.size()>0) {
             String command = queue.poll();
             Runnable runnable = commandMap.get(command);
+            if (runnable == null) {
+                System.out.println(command+" : (경고!) 해당 명령을 수행할 수 없습니다!");
+                stage2.printStageMap();
+                continue;
+            }
             runnable.run();
         }
-        System.out.println("bye~");
+        sc.close();
+        return;
     }
 
     // 지정 stage에 대하여 수행할 작업들을 저장할, commandMap을 생성하여 반환함.
@@ -69,6 +73,13 @@ public class Prompt {
         commandMap.put("S", () -> stage.movePlayerToSouth());
         commandMap.put("D", () -> stage.movePlayerToEast());
         return commandMap;
+    }
+
+    // 사용자로부터 명령을 라인단위로 입력받는다.
+    private Queue<String> inputCommandQueue(Scanner sc) {
+        String inputLine = sc.nextLine().trim().toUpperCase();
+        Queue cmdQueue = new LinkedList(Arrays.asList(inputLine.split("")));
+        return cmdQueue;
     }
 
 }
